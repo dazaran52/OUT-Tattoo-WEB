c7c9ab42165c297d8d58f646093b5b3c96fe358f
dazaran <dazaran52@gmail.com>
2026-05-28 15:58:30 +0200

    Add redirect from /settings to /profile

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,15 @@ const nextConfig = {
   images: {
     domains: [],
   },
+  async redirects() {
+    return [
+      {
+        source: '/settings',
+        destination: '/profile',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
